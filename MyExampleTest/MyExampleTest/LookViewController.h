eb401a499a1b2c97e645771fcdf582f79e777543
chen <2223519001@qq.com>
2013-09-03 23:06:01 +0800

    home
    2

---
+++ MyExampleTest/MyExampleTest/LookViewController.h
@@ -0,0 +1,13 @@
+//
+//  LookViewController.h
+//  MyExampleTest
+//
+//  Created by chen on 13-9-3.
+//  Copyright (c) 2013年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LookViewController : UIViewController
+
+@end
---
+++ MyExampleTest/MyExampleTest/LookViewController.m
@@ -0,0 +1,23 @@
+//
+//  LookViewController.m
+//  MyExampleTest
+//
+//  Created by chen on 13-9-3.
+//  Copyright (c) 2013年 User. All rights reserved.
+//
+
+#import "LookViewController.h"
+
+@interface LookViewController ()
+
+@end
+
+@implementation LookViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
