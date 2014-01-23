c7f74e6bcc10ccd640271ead091f72fe90a74d80
chen <2223519001@qq.com>
2014-01-24 01:36:10 +0800

    142324
    1

--- ShowChenTool/ShowChenTool/test/Test7ViewController.h
+++ ShowChenTool/ShowChenTool/test/Test7ViewController.h
@@ -8,6 +8,62 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum
+{
+    none = 0,
+    add = 1,
+    minus = 2,
+    multiply,
+    divide
+}OperateTag;
+
 @interface Test7ViewController : UIViewController
+
+@end
+
+@interface Operation : NSObject
+
+- (int)GetResult:(NSString *)strNumberA b:(NSString *)strNumberB o:(NSString *)strOperate;
+
+@end
+
+/*
+ 小菜：“哦，你的意思是，我应该把加减乘除等运算分离，修改其中一个不影响另外的几个，增加运算算法也不影响其它代码，是这样吗？”
+ 
+ 大鸟：“自己想去吧，如何用继承和多态，你应该有感觉了。”
+ */
+@interface OperationObj : NSObject
+
+@property (nonatomic, assign) int NumberA;
+
+@property (nonatomic, assign) int NumberB;
+
+@property (nonatomic, assign) NSString *strOperate;
+
+- (void)setA:(NSString *)strNumberA b:(NSString *)strNumberB o:(NSString *)operate;
+
+- (int)GetResult;
+
+@end
+
+@interface OperationAdd : OperationObj
+
+@end
+
+@interface OperationSub : OperationObj
+
+@end
+
+@interface OperationMul : OperationObj
+
+@end
+
+@interface OperationDiv : OperationObj
+
+@end
+
+@interface OperationFactory : NSObject
+
++ (OperationObj *)createOperate:(NSString *)operate;
 
 @end
--- ShowChenTool/ShowChenTool/test/Test7ViewController.m
+++ ShowChenTool/ShowChenTool/test/Test7ViewController.m
@@ -20,9 +20,9 @@
 	// Do any additional setup after loading the view.
     
     //小菜和大鸟的编程故事之一：活字印刷-面向对象思想的先驱
-    NSString *result = [self compute1];
+    NSString *result = [self compute4];
     
-    NSLog(@"%@", result);
+    NSLog(@"结果：%@", result);
 }
 
 - (NSString *)compute1
@@ -58,18 +58,252 @@
     return D;
 }
 
+//小菜和大鸟的编程故事之三：代码规范和重构意识
 - (NSString *)compute2
 {
-//    NSString *B = @"+";
-//    switch (B) {
-//        case @"+":
-//            
-//            break;
-//            
-//        default:
-//            break;
-//    }
-    return nil;
+    NSString *strNumberA = @"1";
+    
+    NSString *strOperate = @"+";
+    
+    NSString *strNumberB = @"2";
+    
+    NSString *result;
+    
+    //小菜和大鸟的编程故事之四：抛弃复制 选择复用
+    /*
+     大鸟：“所有编程初学者都会有这样的问题，就是碰到问题就直觉的用计算机能够理解的逻辑来描述和表达待解决的问题及具体的求解过程。这其实是用计算机的方式去思考，比如计算器这个程序，先要求输入两个数和运算符号，然后根据运算符号判断选择如何运算，得到结果，这本身没有错，但这样的思维却使得我们的程序只为满足实现当前的需求，程序不容易维护，不容易扩展，更不容易复用。从而达不到高质量代码的要求。”
+    */
+    int resultInt = 0;
+    switch ([self switchInt:strOperate])
+    {
+        case add:
+            resultInt = [strNumberA intValue] + [strNumberB intValue];
+            break;
+        case minus:
+            resultInt = [strNumberA intValue] - [strNumberB intValue];
+            break;
+        case multiply:
+            resultInt = [strNumberA intValue] * [strNumberB intValue];
+            break;
+        case divide:
+            resultInt = [strNumberA intValue] / [strNumberB intValue];
+            break;
+            
+        default:
+            break;
+    }
+    result = [NSString stringWithFormat:@"%d", resultInt];
+    
+    return result;
+}
+
+- (int)switchInt:(NSString *)operate
+{
+    int operateInt = none;
+    if ([operate isEqualToString:@"+"])
+        operateInt = add;
+    else if ([operate isEqualToString:@"-"])
+        operateInt = minus;
+    else if ([operate isEqualToString:@"*"])
+        operateInt = multiply;
+    else if ([operate isEqualToString:@"/"])
+        operateInt = divide;
+
+    return operateInt;
+}
+
+- (NSString *)compute3
+{
+    NSString *strNumberA = @"1";
+    
+    NSString *strOperate = @"+";
+    
+    NSString *strNumberB = @"2";
+    
+    NSString *result;
+    
+    //小菜和大鸟的编程故事之五：业务和界面分离
+    /*
+     大鸟：“准确的说，就是让业务逻辑与界面逻辑分开，让它们之间的耦合度下降。只有分离开，才容易达到容易维护或扩展。”
+     */
+    Operation *operation = [[[Operation alloc] init] autorelease];
+    int resultInt = [operation GetResult:strNumberA b:strNumberB o:strOperate];
+    result = [NSString stringWithFormat:@"%d", resultInt];
+    
+    return result;
+}
+
+- (NSString *)compute4
+{
+    NSString *strNumberA = @"1";
+    
+    NSString *strOperate = @"+";
+    
+    NSString *strNumberB = @"2";
+    
+    NSString *result;
+    
+    //小菜和大鸟的编程故事之六：简单工厂模式之体验
+    /*
+     大鸟：“写得很不错吗，大大超出我的想象了，你现在的问题其实就是如何去实例化对象的问题，哈，今天心情不错，再教你一招‘简单工厂模式’，也就是说，到底要实例化谁，将来会不会增加实例化的对象（比如增加开根运算），这是很容易变化的地方，应该考虑用一个单独的类来做这个创造实例的过程，这就是工厂，来，我们看看这个类如何写。”
+     */
+    OperationObj *oper = [OperationFactory createOperate:strOperate];
+    [oper setA:strNumberA b:strNumberB o:strOperate];
+    int resultInt = [oper GetResult];
+    result = [NSString stringWithFormat:@"%d", resultInt];
+    
+    return result;
+}
+
+@end
+
+//封装
+@implementation Operation
+
+- (int)GetResult:(NSString *)strNumberA b:(NSString *)strNumberB o:(NSString *)strOperate
+{
+    int resultInt = 0;
+    switch ([self switchInt:strOperate])
+    {
+        case add:
+            resultInt = [strNumberA intValue] + [strNumberB intValue];
+            break;
+        case minus:
+            resultInt = [strNumberA intValue] - [strNumberB intValue];
+            break;
+        case multiply:
+            resultInt = [strNumberA intValue] * [strNumberB intValue];
+            break;
+        case divide:
+            resultInt = [strNumberA intValue] / [strNumberB intValue];
+            break;
+            
+        default:
+            break;
+    }
+    return resultInt;
+}
+
+- (int)switchInt:(NSString *)operate
+{
+    int operateInt = none;
+    if ([operate isEqualToString:@"+"])
+        operateInt = add;
+    else if ([operate isEqualToString:@"-"])
+        operateInt = minus;
+    else if ([operate isEqualToString:@"*"])
+        operateInt = multiply;
+    else if ([operate isEqualToString:@"/"])
+        operateInt = divide;
+    
+    return operateInt;
+}
+
+@end
+
+//继承和多态
+@implementation OperationObj
+
+@synthesize NumberA = _NumberA;
+@synthesize NumberB = _NumberB;
+@synthesize strOperate = _strOperate;
+
+- (void)dealloc
+{
+    [_strOperate release];
+    [super dealloc];
+}
+
+- (void)setA:(NSString *)strNumberA b:(NSString *)strNumberB o:(NSString *)operate
+{
+    _NumberA = [strNumberA intValue];
+    _NumberB = [strNumberB intValue];
+    _strOperate = [operate retain];
+}
+
+- (int)GetResult
+{
+    return 0;
+}
+
+@end
+
+@implementation OperationAdd
+
+- (int)GetResult
+{
+    return self.NumberA + self.NumberB;
+}
+
+@end
+
+@implementation OperationSub
+
+- (int)GetResult
+{
+    return self.NumberA - self.NumberB;
+}
+
+@end
+
+@implementation OperationMul
+
+- (int)GetResult
+{
+    return self.NumberA * self.NumberB;
+}
+
+@end
+
+@implementation OperationDiv
+
+- (int)GetResult
+{
+    return self.NumberA / self.NumberB;
+}
+
+@end
+
+@implementation OperationFactory
+
++ (OperationObj *)createOperate:(NSString *)operate
+{
+    OperationObj *oper = nil;
+    
+    switch ([self switchInt:operate])
+    {
+        case add:
+            oper = [[OperationAdd alloc] init];
+            break;
+        case minus:
+            oper = [[OperationSub alloc] init];
+            break;
+        case multiply:
+            oper = [[OperationMul alloc] init];
+            break;
+        case divide:
+            oper = [[OperationDiv alloc] init];
+            break;
+            
+        default:
+            break;
+    }
+    return [oper autorelease];
+}
+
++ (int)switchInt:(NSString *)operate
+{
+    int operateInt = none;
+    if ([operate isEqualToString:@"+"])
+        operateInt = add;
+    else if ([operate isEqualToString:@"-"])
+        operateInt = minus;
+    else if ([operate isEqualToString:@"*"])
+        operateInt = multiply;
+    else if ([operate isEqualToString:@"/"])
+        operateInt = divide;
+    
+    return operateInt;
 }
 
 @end
